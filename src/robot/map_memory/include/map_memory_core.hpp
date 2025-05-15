d02126e3ecaf71004e45eda7c58f48a818328c1d
allenngkc <pikachuexeallen@gmail.com>
2025-05-15 00:09:39 -0400

    only include declarations in map memory hpp

--- src/robot/map_memory/include/map_memory_core.hpp
+++ src/robot/map_memory/include/map_memory_core.hpp
@@ -1,59 +1,36 @@
 #ifndef MAP_MEMORY_CORE_HPP_
 #define MAP_MEMORY_CORE_HPP_
 
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "nav_msgs/msg/odometry.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <chrono>
+#include <cmath>
 
 namespace robot
 {
 
-class MapMemoryCore {
-  public:
-    explicit MapMemoryCore(const rclcpp::Logger& logger);
-    
-    void initMapMemory(double resolution, int width, int height, const geometry_msgs::msg::Pose origin);
-    void integrateCostmap();
+class MapMemoryCore
+{
+public:
+  MapMemoryCore(const rclcpp::Logger& logger);
 
-    void setCostmapUpdated(bool val);
-    void setShouldUpdateMap(bool val);
-    void setlatestCostmap(nav_msgs::msg::OccupancyGrid msg);
-    void setlatestOdom(nav_msgs::msg::Odometry msg);
-    void setLastX(double x);
-    void setLastY(double y);
+  // Global map and robot position
+  nav_msgs::msg::OccupancyGrid global_map_;
+  double last_x;
+  double last_y;
+  double last_yaw;
+  const double distance_threshold;
 
-    double getLastX();
-    double getLastY();
-    double getDistanceThreshold();
-    bool getCostmapUpdate();
-    bool getShouldUpdateMap();
-    nav_msgs::msg::OccupancyGrid getGlobalMap();
+  // Integrate the latest costmap into the global map
+  void integrateCostmap();
 
-    double last_yaw = 0.0;
+  // Flags
+  nav_msgs::msg::OccupancyGrid latest_costmap_;
 
-  private:
-    rclcpp::Logger logger_;
-    nav_msgs::msg::OccupancyGrid global_map_;
-
-    // global_map_.header.stamp = rclcpp::Time(0);
-    // global_map_.header.frame_id = "sim_world";
-    // global_map_.info.resolution = 0.1; 
-    // global_map_.info.width = 100;       
-    // global_map_.info.height = 100;    
-    // global_map_.info.origin.position.x = -20.0;
-    // global_map_.info.origin.position.y = -20.0;
-    // global_map_.info.origin.position.z = 0.0;
-    // global_map_.info.origin.orientation.w = 1.0;
-    // global_map_.data.resize(global_map_.info.width * global_map_.info.height, -1);
-    
-    nav_msgs::msg::OccupancyGrid latest_costmap_;
-    nav_msgs::msg::Odometry latest_odom_;
-    double last_x, last_y;
-    double distance_threshold = 1.5;
-    bool costmap_updated_ = false;
-    bool should_update_map_ = false;
+private:
+  rclcpp::Logger logger_;
 };
 
-}  
-
+}
 #endif  
--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -2,8 +2,6 @@
 #define MAP_MEMORY_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "nav_msgs/msg/odometry.hpp"
 
 #include "map_memory_core.hpp"
 
@@ -12,18 +10,25 @@ class MapMemoryNode : public rclcpp::Node {
     MapMemoryNode();
 
     void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+
     void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
 
     void updateMap();
 
-  private:
-    robot::MapMemoryCore map_memory_;
+    void publishMap();
 
+  private:
+    // Subscribers and Publisher
     rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
-    
+
+    bool map_update_done_;  
+    bool costmap_updated_;
+    bool should_update_map_;
+
+    robot::MapMemoryCore map_memory_;
 };
 
 #endif 
